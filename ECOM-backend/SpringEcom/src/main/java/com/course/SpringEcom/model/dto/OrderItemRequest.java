88fd6de93a5b9cbdb00753cf8c703ea1f491c4da
Er Choudhary <godarachoudhary1111@gmail.com>
2026-05-14 20:38:49 +0530

    full stack project frontend + backend

---
+++ ECOM-backend/SpringEcom/src/main/java/com/course/SpringEcom/model/dto/OrderItemRequest.java
@@ -0,0 +1,9 @@
+package com.course.SpringEcom.model.dto;
+
+public record OrderItemRequest(
+		int productId,
+		int quantity
+)
+{
+	
+}
---
+++ ECOM-backend/SpringEcom/src/main/java/com/course/SpringEcom/repo/OrderRepo.java
@@ -0,0 +1,13 @@
+package com.course.SpringEcom.repo;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.course.SpringEcom.model.Order;
+
+@Repository
+public interface OrderRepo extends JpaRepository<Order, Integer>{
+	Optional<Order> findByOrderId(String orderId);
+}
